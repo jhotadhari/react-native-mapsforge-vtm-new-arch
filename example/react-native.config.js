3321b829dc7197d425d01fc03dd19f1106c1001f
jhotadhari <tellme@waterproof-webdesign.de>
2025-04-16 12:47:26 +0200

    Remove ios

--- example/react-native.config.js
+++ example/react-native.config.js
@@ -2,18 +2,13 @@ const path = require('path');
 const pkg = require('../package.json');
 
 module.exports = {
-  project: {
-    ios: {
-      automaticPodsInstallation: true,
-    },
-  },
+  project: {},
   dependencies: {
     [pkg.name]: {
       root: path.join(__dirname, '..'),
       platforms: {
         // Codegen script incorrectly fails without this
         // So we explicitly specify the platforms with empty object
-        ios: {},
         android: {},
       },
     },
